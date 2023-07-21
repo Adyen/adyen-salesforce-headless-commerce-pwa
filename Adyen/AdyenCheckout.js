1feda98db89daecfd81979208b9a3363274eaf17
Shani <31096696+shanikantsingh@users.noreply.github.com>
2023-07-21 10:16:01 +0200

    Feature/render dropin pwa (#4)
    * setup dropin integration
    * setup sessions call to Adyen in ssr.js
    * fetch basket in ssr.js
    
    ---------
    
    Co-authored-by: zaid <zaid.dreakh@adyen.com>

---
+++ Adyen/AdyenCheckout.js
@@ -0,0 +1,82 @@
+import React, {useEffect, useRef, useState} from 'react'
+import {useAccessToken, useCustomerId} from '@salesforce/commerce-sdk-react'
+import {useCurrentCustomer} from '@salesforce/retail-react-app/app/hooks/use-current-customer'
+import {useCurrentBasket} from '@salesforce/retail-react-app/app/hooks/use-current-basket'
+import {useCheckout} from '@salesforce/retail-react-app/app/pages/checkout/util/checkout-context'
+import AdyenCheckout from '@adyen/adyen-web'
+import '@adyen/adyen-web/dist/adyen.css'
+const AdyenCheckoutComponent = () => {
+    const {data: customer} = useCurrentCustomer()
+    const {data: basket} = useCurrentBasket()
+    const {step, STEPS} = useCheckout()
+    const {getTokenWhenReady} = useAccessToken()
+    const customerId = useCustomerId()
+    const [payment, setPayment] = useState({})
+    const paymentContainer = useRef(null)
+    useEffect(() => {
+        const fetchSession = async () => {
+            if (step === STEPS.PAYMENT) {
+                const token = await getTokenWhenReady()
+                fetch(`/sessions`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        authorization: `Bearer ${token}`,
+                        customerid: customerId
+                    }
+                }).then((res) => {
+                    if (res.status >= 300) {
+                        setPayment({error: res})
+                    } else {
+                        res.json().then((data) => {
+                            setPayment(data[0])
+                            createCheckout(data[0])
+                        })
+                    }
+                })
+            }
+        }
+        fetchSession()
+    }, [step, STEPS, basket])
+
+    useEffect(() => {
+        if (payment?.error) {
+            // navigate(`/status/error?reason=${error}`, { replace: true });
+            console.log(payment?.error)
+        }
+    }, [payment])
+
+    const createCheckout = async (payment) => {
+        const checkout = await AdyenCheckout({
+            environment: payment.ADYEN_ENVIRONMENT,
+            clientKey: payment.ADYEN_CLIENT_KEY,
+            showPayButton: true,
+            session: {
+                id: payment.id,
+                sessionData: payment.sessionData
+            },
+            paymentMethodsConfiguration: {
+                card: {
+                    hasHolderName: true,
+                    holderNameRequired: true,
+                    billingAddressRequired: true
+                }
+            },
+            onPaymentCompleted: (response, _component) =>
+                // navigate(getRedirectUrl(response.resultCode), { replace: true }),
+                console.log(response.resultCode),
+            onError: (error, _component) => {
+                //navigate(`/status/error?reason=${error.message}`, { replace: true });
+                console.log(error)
+            }
+        })
+
+        if (paymentContainer.current) {
+            checkout.create('dropin').mount(paymentContainer.current)
+        }
+    }
+
+    return <div ref={paymentContainer} className="payment"></div>
+}
+
+export default AdyenCheckoutComponent
---
+++ overrides/app/ssr.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
+
+const path = require('path')
+const {getRuntime} = require('@salesforce/pwa-kit-runtime/ssr/server/express')
+const {isRemote} = require('@salesforce/pwa-kit-runtime/utils/ssr-server')
+const {getConfig} = require('@salesforce/pwa-kit-runtime/utils/ssr-config')
+const {ShopperCustomers} = require('commerce-sdk-isomorphic')
+const helmet = require('helmet')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {Client, Config, CheckoutAPI} = require('@adyen/api-library')
+require('dotenv').config()
+
+const options = {
+    // The build directory (an absolute path)
+    buildDir: path.resolve(process.cwd(), 'build'),
+
+    // The cache time for SSR'd pages (defaults to 600 seconds)
+    defaultCacheTimeSeconds: 600,
+
+    // This is the value of the 'mobify' object from package.json
+    mobify: getConfig(),
+
+    // The port that the local dev server listens on
+    port: 3000,
+
+    // The protocol on which the development Express app listens.
+    // Note that http://localhost is treated as a secure context for development.
+    protocol: 'http'
+}
+
+const runtime = getRuntime()
+
+const {handler} = runtime.createHandler(options, (app) => {
+    app.use(bodyParser.json())
+
+    const config = new Config()
+    config.apiKey = process.env.ADYEN_API_KEY //REPLACE With YOUR API KEY
+    const client = new Client({config})
+    client.setEnvironment(process.env.ADYEN_ENVIRONMENT)
+    const checkout = new CheckoutAPI(client)
+
+    app.post('/sessions', async (req, res) => {
+        try {
+            const {app: appConfig} = getConfig()
+            const shopperCustomers = new ShopperCustomers({
+                ...appConfig.commerceAPI,
+                headers: {authorization: req.headers.authorization}
+            })
+
+            const {baskets} = await shopperCustomers.getCustomerBaskets({
+                parameters: {
+                    customerId: req.headers.customerid
+                }
+            })
+
+            const response = await checkout.sessions({
+                countryCode: 'US',
+                amount: {
+                    value: baskets[0].orderTotal,
+                    currency: baskets[0].currency
+                },
+                reference: baskets[0].basketId, // todo: check if its valid to pass basketID
+                merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT, //todo: REPLACE With YOUR MERCHANT ACCOUNT
+                returnUrl: process.env.HOST_URL //todo: REPLACE with a proper value  // required for 3ds2 redirect flow
+            })
+
+            res.json([
+                {
+                    ...response,
+                    ADYEN_CLIENT_KEY: process.env.ADYEN_CLIENT_KEY,
+                    ADYEN_ENVIRONMENT: process.env.ADYEN_ENVIRONMENT
+                }
+            ]) // sending a tuple with orderRef as well to inform about the unique order reference
+        } catch (err) {
+            res.status(err.statusCode).json(err.message)
+        }
+    })
+
+    // Set HTTP security headers
+    app.use(
+        helmet({
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    'img-src': ["'self'", '*.commercecloud.salesforce.com', 'data:', '*.adyen.com'],
+                    'script-src': ["'self'", "'unsafe-eval'", 'storage.googleapis.com'],
+                    'connect-src': ["'self'", 'api.cquotient.com', '*.adyen.com'],
+                    'frame-src': ["'self'", '*.adyen.com'],
+
+                    // Do not upgrade insecure requests for local development
+                    'upgrade-insecure-requests': isRemote() ? [] : null
+                }
+            },
+            hsts: isRemote()
+        })
+    )
+
+    // Handle the redirect from SLAS as to avoid error
+    app.get('/callback?*', (req, res) => {
+        res.send()
+    })
+    app.get('/robots.txt', runtime.serveStaticFile('static/robots.txt'))
+
+    app.get('/favicon.ico', runtime.serveStaticFile('static/ico/favicon.ico'))
+
+    app.get('/worker.js(.map)?', runtime.serveServiceWorker)
+    app.get('*', runtime.render)
+})
+// SSR requires that we export a single handler function called 'get', that
+// supports AWS use of the server that we created above.
+exports.get = handler
