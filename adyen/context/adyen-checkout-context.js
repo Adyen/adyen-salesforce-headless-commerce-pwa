05ff7e7465847af641fe891dab2e36a23914ceb2
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-07-27 14:50:53 +0200

    fix: removed salesforce comment

--- adyen/context/adyen-checkout-context.js
+++ adyen/context/adyen-checkout-context.js
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2023, Salesforce, Inc.
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import {useAccessToken, useCustomerId} from '@salesforce/commerce-sdk-react'
