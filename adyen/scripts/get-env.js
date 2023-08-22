c525e64e1f977114312cab799d873b4579b48fe2
Shani <shani.singh@adyen.com>
2023-08-22 10:58:46 +0200

    feat: script to add .env file to mrt env-var

---
+++ adyen/scripts/get-env.js
@@ -0,0 +1,23 @@
+const {
+    readCredentials,
+    DEFAULT_CLOUD_ORIGIN,
+    getCredentialsFile
+} = require('@salesforce/pwa-kit-dev/utils/script-utils')
+const {CloudAPIClientCustom} = require('./cloudAPICilent')
+const dotenv = require('dotenv')
+;(async function () {
+    const result = dotenv.config()
+
+    if (result.error) {
+        throw result.error
+    }
+
+    const env = result.parsed
+    const credentials = await readCredentials(getCredentialsFile(DEFAULT_CLOUD_ORIGIN))
+    const opts = {credentials, projectID: env.projectID, environmentID: env.environmentID}
+    const client = new CloudAPIClientCustom(opts)
+    const data = await client.getEnv()
+    const warnings = data.warnings || []
+    warnings.forEach((warn) => console.log(warn))
+    console.log('env vars fetched!', data)
+})()
