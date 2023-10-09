ab657db1314cc31c658a6959c0504fa06c5f5990
Shani <shani.singh@adyen.com>
2023-10-09 10:13:11 +0200

    feature: pass limeItems for openInvoiceMethods

--- adyen/controllers/payments.js
+++ adyen/controllers/payments.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict'
-import {formatAddressInAdyenFormat} from "../utils/formatAddress.mjs";
-import {getCurrencyValueForApi} from "../utils/parsers.mjs";
-import {APPLICATION_VERSION} from "../utils/constants.mjs";
-import {createCheckoutResponse} from "../utils/createCheckoutResponse.mjs"
+import {formatAddressInAdyenFormat} from '../utils/formatAddress.mjs'
+import {getCurrencyValueForApi} from '../utils/parsers.mjs'
+import {APPLICATION_VERSION} from '../utils/constants.mjs'
+import {createCheckoutResponse} from '../utils/createCheckoutResponse.mjs'
 
 const {CheckoutAPI, Client, Config} = require('@adyen/api-library')
 const {ShopperOrders} = require('commerce-sdk-isomorphic')
@@ -40,7 +40,7 @@ async function sendPayments(req, res) {
         }
 
         const {data} = req.body
-        const response = await checkout.payments({
+        const paymentRequest = {
             ...data,
             billingAddress: formatAddressInAdyenFormat(order.billingAddress),
             deliveryAddress: formatAddressInAdyenFormat(order.shipments[0].shippingAddress),
@@ -53,12 +53,16 @@ async function sendPayments(req, res) {
             applicationInfo: getApplicationInfo(),
             authenticationData: {
                 threeDSRequestData: {
-                    nativeThreeDS: 'preferred',
+                    nativeThreeDS: 'preferred'
                 }
             },
             channel: 'Web',
-            returnUrl: `${data.origin}/checkout`,
-        })
+            returnUrl: `${data.origin}/checkout`
+        }
+        if (isOpenInvoiceMethod(data?.paymentMethod?.type)) {
+            paymentRequest.lineItems = getLineItems(order)
+        }
+        const response = await checkout.payments(paymentRequest)
 
         res.json(createCheckoutResponse(response))
     } catch (err) {
@@ -78,5 +82,54 @@ function getApplicationInfo() {
             integrator: process.env.SYSTEM_INTEGRATOR_NAME
         }
     }
+}
+
+function isOpenInvoiceMethod(paymentMethod) {
+    return (
+        paymentMethod.indexOf('afterpay') > -1 ||
+        paymentMethod.indexOf('klarna') > -1 ||
+        paymentMethod.indexOf('ratepay') > -1 ||
+        paymentMethod.indexOf('facilypay') > -1 ||
+        paymentMethod === 'zip' ||
+        paymentMethod === 'affirm' ||
+        paymentMethod === 'clearpay'
+    )
+}
+
+function getLineItems(order) {
+    const productLineItems = order?.productItems.map((productItem) => {
+        return {
+            id: productItem.itemId,
+            quantity: productItem.quantity,
+            description: productItem.itemText,
+            amountExcludingTax: getCurrencyValueForApi(productItem.basePrice, order.currency),
+            taxAmount: getCurrencyValueForApi(productItem.tax, order.currency),
+            taxCategory: productItem.taxClassId,
+            taxPercentage: productItem.taxRate
+        }
+    })
+    const shippingLineItems = order?.shippingItems.map((shippingItem) => {
+        return {
+            id: shippingItem.itemId,
+            quantity: 1,
+            description: shippingItem.itemText,
+            amountExcludingTax: getCurrencyValueForApi(shippingItem.basePrice, order.currency),
+            taxAmount: getCurrencyValueForApi(shippingItem.tax, order.currency),
+            taxCategory: shippingItem.taxClassId,
+            taxPercentage: shippingItem.taxRate
+        }
+    })
+    const priceAdjustmentLineItems = order?.priceAdjustments.map((priceAdjustment) => {
+        return {
+            id: priceAdjustment.priceAdjustmentId,
+            quantity: priceAdjustment.quantity,
+            description: priceAdjustment.itemText,
+            amountExcludingTax: getCurrencyValueForApi(priceAdjustment.basePrice, order.currency),
+            taxAmount: getCurrencyValueForApi(priceAdjustment.tax, order.currency),
+            taxCategory: priceAdjustment.taxClassId || 'None',
+            taxPercentage: priceAdjustment.taxRate
+        }
+    })
+    return [...productLineItems, ...shippingLineItems, ...priceAdjustmentLineItems]
 }
 module.exports = {sendPayments}
