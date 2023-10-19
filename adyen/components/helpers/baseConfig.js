9709cfa5f1886492d1f2d65ea173a0b73a90c6ef
Shani <31096696+shanikantsingh@users.noreply.github.com>
2023-10-19 11:03:10 +0200

    Feature/payment methods configuration (#26)
    * refactor: paymentMethods configuration
    
    * refactor: paymentMethods configuration

---
+++ adyen/components/helpers/baseConfig.js
@@ -0,0 +1,44 @@
+import {AdyenPaymentsService} from '../../services/payments'
+import {AdyenPaymentsDetailsService} from '../../services/payments-details'
+export const baseConfig = (props) => {
+    const onSubmit = async (state, component) => {
+        try {
+            if (!state.isValid) throw new Error('invalid state')
+            const adyenPaymentService = new AdyenPaymentsService(props?.token)
+            const paymentsResponse = await adyenPaymentService.submitPayment(
+                state.data,
+                props.basketId,
+                props?.customerId
+            )
+            if (paymentsResponse?.isSuccessful) {
+                props?.successHandler(paymentsResponse.merchantReference)
+            } else if (paymentsResponse?.action) {
+                await component.handleAction(paymentsResponse.action)
+            }
+        } catch (error) {
+            props?.errorHandler(error)
+        }
+    }
+    const onAdditionalDetails = async (state, component) => {
+        try {
+            if (!state.isValid) throw new Error('invalid state')
+            const adyenPaymentsDetailsService = new AdyenPaymentsDetailsService(props?.token)
+            const paymentsDetailsResponse = await adyenPaymentsDetailsService.submitPaymentsDetails(
+                state.data,
+                props?.customerId
+            )
+            if (paymentsDetailsResponse?.isSuccessful) {
+                props?.successHandler(paymentsDetailsResponse.merchantReference)
+            } else if (paymentsDetailsResponse?.action) {
+                await component.handleAction(paymentsDetailsResponse.action)
+            }
+        } catch (error) {
+            props?.errorHandler(error)
+        }
+    }
+
+    return {
+        onSubmit: onSubmit,
+        onAdditionalDetails: onAdditionalDetails
+    }
+}
---
+++ adyen/components/paymentMethodsConfiguration.js
@@ -0,0 +1,24 @@
+import {baseConfig} from './helpers/baseConfig'
+import {klarnaConfig} from './klarna/config'
+import {cardConfig} from './card/config'
+import {paypalConfig} from './paypal/config'
+
+export const paymentMethodsConfiguration = ({paymentMethods, ...props}) => {
+    const defaultConfig = baseConfig(props)
+    const paymentMethodsConfig = {
+        card: cardConfig(props),
+        klarna: klarnaConfig(props),
+        klarna_account: klarnaConfig(props),
+        klarna_paynow: klarnaConfig(props),
+        paypal: paypalConfig(props)
+    }
+
+    return Object.fromEntries(
+        paymentMethods.map((paymentMethod) => {
+            const type = paymentMethod.type === 'scheme' ? 'card' : paymentMethod.type
+            return Object.hasOwn(paymentMethodsConfig, type)
+                ? [type, paymentMethodsConfig[type]]
+                : [type, defaultConfig]
+        })
+    )
+}
