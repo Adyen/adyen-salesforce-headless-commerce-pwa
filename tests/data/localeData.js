b82c4a412ddc825d7abbb6ccb6fd3dc1dd1558ac
Cenk Kucukiravul <cenkiravul35@gmail.com>
2023-11-09 17:26:14 +0100

    Add French locale

--- tests/data/localeData.js
+++ tests/data/localeData.js
@@ -10,4 +10,15 @@ export class LocaleData {
             proceedToCheckoutButtonCaption: 'Proceed to Checkout'
         }
     }
+    FR = {
+        lang: 'fr-FR',
+        landingPage: {
+            heading: 'React PWA Starter Store pour le retail'
+        },
+        productDetailPage: {
+            productColor: 'Gris',
+            addToCartButtonCaption: 'Ajouter au panier',
+            proceedToCheckoutButtonCaption: 'Passer au checkout'
+        }
+    }
 }
