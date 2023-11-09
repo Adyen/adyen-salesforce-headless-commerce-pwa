909ea6743858bba40ad3367d2352a75fe10f49fe
Cenk Kucukiravul <cenkiravul35@gmail.com>
2023-11-09 17:23:17 +0100

    Add locale variables

---
+++ tests/data/localeData.js
@@ -0,0 +1,13 @@
+export class LocaleData {
+    EN = {
+        lang: 'en-US',
+        landingPage: {
+            heading: 'The React PWA Starter Store for Retail'
+        },
+        productDetailPage: {
+            productColor: 'Gray',
+            addToCartButtonCaption: 'Add to Cart',
+            proceedToCheckoutButtonCaption: 'Proceed to Checkout'
+        }
+    }
+}
