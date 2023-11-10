8be0b8e02310ac1ab885c70deacba2ce616c20f1
Cenk Kucukiravul <cenkiravul35@gmail.com>
2023-11-11 00:44:33 +0100

    Create payment helper and add CC payments function

---
+++ tests/helpers/PaymentHelper.js
@@ -0,0 +1,48 @@
+export class PaymentHelper {
+    constructor(page) {
+        this.page = page
+        this.paymentSection = this.page.locator('.sf-toggle-card-step-3-content')
+
+        this.activePaymentType = this.page.locator('.adyen-checkout__payment-method--selected')
+
+        // CC Component Locators
+        this.holderNameInput = this.activePaymentType.locator(
+            '.adyen-checkout__card__holderName input'
+        )
+        this.cardNumberInput = this.activePaymentType
+            .frameLocator('.adyen-checkout__card__cardNumber__input iframe')
+            .locator('.input-field')
+        this.expDateInput = this.activePaymentType
+            .frameLocator('.adyen-checkout__card__exp-date__input iframe')
+            .locator('.input-field')
+        this.cvcInput = this.activePaymentType
+            .frameLocator('.adyen-checkout__card__cvc__input iframe')
+            .locator('.input-field')
+    }
+
+    async selectPaymentType(paymentType) {
+        await this.page
+            .locator(
+                `//*[contains(@class, 'adyen-checkout__payment-method__name') and contains(text(), '${paymentType}')]`
+            )
+            .click()
+    }
+
+    async fillInput(inputField, value) {
+        await inputField.click()
+        await inputField.type(value, {delay: 50})
+    }
+
+    // CC
+    async fillCreditCardInfo(cardHolderName, cardNumber, cardExpirationDate, cardCVC = undefined) {
+        await this.fillInput(this.cardNumberInput, cardNumber)
+        await this.fillInput(this.expDateInput, cardExpirationDate)
+        if (cardCVC !== undefined) {
+            await this.fillInput(this.cvcInput, cardCVC)
+        }
+        await this.fillInput(
+            this.holderNameInput,
+            cardHolderName.firstName + ' ' + cardHolderName.lastName
+        )
+    }
+}
