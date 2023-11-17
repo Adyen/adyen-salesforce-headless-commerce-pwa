afa1e22ff00af7c05b3457ccd4ad3f43628594df
Aleksandar Mihajlovski <a.mihajlovski91@gmail.com>
2023-11-17 16:38:49 +0100

    Payment card (#41)
    * feat: payment card modifications
    
    * fix: remove on bin lookup

--- adyen/components/card/config.js
+++ adyen/components/card/config.js
@@ -4,6 +4,7 @@ export const cardConfig = (props) => {
     const isRegistered = props?.customerType?.isRegistered
     return {
         ...baseConfig(props),
+        _disableClickToPay: true,
         showPayButton: true,
         hasHolderName: true,
         holderNameRequired: true,
