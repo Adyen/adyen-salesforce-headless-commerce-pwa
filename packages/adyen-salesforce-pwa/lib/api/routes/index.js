be32b8a9e473ade8556c4a64051e7f3f70da9e3f
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-11-30 16:20:24 +0100

    fix: conflicts

--- packages/adyen-salesforce-pwa/lib/api/routes/index.js
+++ packages/adyen-salesforce-pwa/lib/api/routes/index.js
@@ -17,10 +17,7 @@ function ErrorHandler(err, req, res) {
     Logger.error(err.message, err.cause)
     res.status(err.statusCode || 500).json(createErrorResponse(err.message))
 }
-<<<<<<<< HEAD:packages/adyen-salesforce-pwa/lib/api/routes/index.js
 
-========
->>>>>>>> develop:packages/adyen-salesforce-pwa/lib/api/routes/router.js
 function registerAdyenEndpoints(app, runtime, overrides) {
     const environmentHandler = overrides?.environment || [
         EnvironmentController,
@@ -64,8 +61,4 @@ function registerAdyenEndpoints(app, runtime, overrides) {
     app.post('/api/adyen/webhook', ...webhookHandler)
 }
 
-<<<<<<<< HEAD:packages/adyen-salesforce-pwa/lib/api/routes/index.js
 export {registerAdyenEndpoints, SuccessHandler, ErrorHandler}
-========
-export default registerAdyenEndpoints
->>>>>>>> develop:packages/adyen-salesforce-pwa/lib/api/routes/router.js
