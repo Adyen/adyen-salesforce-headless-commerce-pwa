281b8e96324df8dc3630c730062f15b7418bea51
Shani <31096696+shanikantsingh@users.noreply.github.com>
2023-12-05 15:31:56 +0530

    Release/1.0.0-beta.2 (#72)
    * [SFI-454 & SFI-328] Set up e2e framework & Add GitHub Actions file (#42)
    
    * Initial commit
    
    * feat: added endpoint for e2e setup
    
    * fix: removed unused config
    
    * Add shopper data
    
    * Add card data
    
    * Add scenario helper class
    
    * Add locale variables
    
    * Add French locale
    
    * Define default locale and update shipping locators
    
    * Add successful order confirmation message
    
    * Add successful order verification
    
    * Create payment helper and add CC payments function
    
    * Update base URL
    
    * Add generic pay button
    
    * Add e2e scenario for CC
    
    * Add 3Ds2 locators
    
    * Create draft workflow file
    
    * Add secrets and clean up
    
    * fix: removed types module
    
    * fix: tests
    
    * Update e2e.yml
    
    Bump the timeout of test workflow to 10 mins
    
    * Enable larger runners for E2E pipeline
    
    * Update workflow timeout to 20 mins
    
    * Add webserver configuration
    
    * Add npm commands
    
    * Merge test step with webserver init
    
    * Reduce timeout to 10 mins
    
    * Convert npm ci to npm i due to removal of package.lock.json
    
    * Update Product color according to the catalog
    
    * Add initial setup checker scenario
    
    * Add setup step
    
    * fix: update workflow for e2e
    
    * fix: start webserver
    
    * fix: use link instead of install
    
    * fix: build then link
    
    ---------
    
    Co-authored-by: Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
    Co-authored-by: Aleksandar Mihajlovski <a.mihajlovski91@gmail.com>
    
    * fix: export country list and currency list (#66)
    
    * fix: import country list as named import (#67)
    
    * chore: added prepare dev script (#68)
    
    * chore: add Zenit and Shubham as code owners (#69)
    
    * chore: bump version (#71)
    
    ---------
    
    Co-authored-by: Cenk Kucukiravul <cenkiravul35@gmail.com>
    Co-authored-by: Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
    Co-authored-by: Aleksandar Mihajlovski <a.mihajlovski91@gmail.com>

---
+++ tests/data/cardData.js
@@ -0,0 +1,32 @@
+export class CardData {
+    noThreeDs = {
+        holderName: 'John Doe',
+        cardNumber: '370000000000002',
+        expirationDate: '0330',
+        cvc: '7373'
+    }
+    threeDs1 = {
+        holderName: 'John Doe',
+        cardNumber: '345177925488348',
+        expirationDate: '0330',
+        cvc: '7373'
+    }
+    threeDs2 = {
+        holderName: 'John Doe',
+        cardNumber: '4212345678910006',
+        expirationDate: '0330',
+        cvc: '737'
+    }
+    coBrandedBCMC = {
+        holderName: 'John Doe',
+        cardNumber: '4871049999999910',
+        expirationDate: '0330',
+        cvc: '737'
+    }
+
+    giftCard = {
+        cardNumber: '6036280000000000000',
+        pin: '123',
+        brand: 'givex'
+    }
+}
---
+++ tests/data/localeData.js
@@ -0,0 +1,26 @@
+export class LocaleData {
+    EN = {
+        lang: 'en-US',
+        landingPage: {
+            heading: 'The React PWA Starter Store for Retail'
+        },
+        productDetailPage: {
+            productColor: 'Gray',
+            addToCartButtonCaption: 'Add to Cart',
+            proceedToCheckoutButtonCaption: 'Proceed to Checkout'
+        },
+        successfulOrderMessage: 'Thank you for your order!'
+    }
+    FR = {
+        lang: 'fr-FR',
+        landingPage: {
+            heading: 'React PWA Starter Store pour le retail'
+        },
+        productDetailPage: {
+            productColor: 'Gris',
+            addToCartButtonCaption: 'Ajouter au panier',
+            proceedToCheckoutButtonCaption: 'Passer au checkout'
+        },
+        successfulOrderMessage: 'Thank you for your order!'
+    }
+}
