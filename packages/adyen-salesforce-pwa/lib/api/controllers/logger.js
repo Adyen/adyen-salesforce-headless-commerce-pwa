a16ddaa2e5180ad96b9c46ea7adf4bfbd523f696
Shani <shani.singh@adyen.com>
2023-12-08 22:13:14 +0530

    chore: refactor logger.js

--- packages/adyen-salesforce-pwa/lib/api/controllers/logger.js
+++ packages/adyen-salesforce-pwa/lib/api/controllers/logger.js
@@ -4,20 +4,29 @@ const ADYEN_PREFIX = 'ADYEN'
 
 class Logger {
     static info(step, message) {
-        console.info(`${ADYEN_PREFIX}_INFO ${APPLICATION_VERSION} ${step} ${message}`)
+        console.info(composeLog('INFO', step, message))
     }
 
     static warn(step, message) {
-        console.warn(`${ADYEN_PREFIX}_WARN ${APPLICATION_VERSION} ${step} ${message}`)
+        console.warn(composeLog('WARN', step, message))
     }
 
     static error(step, message) {
-        console.error(`${ADYEN_PREFIX}_ERROR ${APPLICATION_VERSION} ${step} ${message}`)
+        console.error(composeLog('ERROR', step, message))
     }
 
     static debug(step, message) {
-        console.debug(`${ADYEN_PREFIX}_DEBUG ${APPLICATION_VERSION} ${step} ${message}`)
+        console.debug(composeLog('DEBUG', step, message))
     }
+}
+
+const composeLog = (type, step, message) => {
+    const logMessage = message instanceof Object ? JSON.stringify(message) : message
+    return `${ADYEN_PREFIX}_${type} ${APPLICATION_VERSION} ${step ? step : ''} ${
+        logMessage ? logMessage : ''
+    }`
+        .trim()
+        .replace(/\s\s+/g, ' ')
 }
 
 export default Logger
