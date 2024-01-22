c388c700216d05d34ff14c9520ea6425fee1bccb
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2024-01-22 11:09:46 +0100

    feat: added more unit tests for amazon config

--- packages/adyen-salesforce-pwa/lib/components/amazonpay/config.js
+++ packages/adyen-salesforce-pwa/lib/components/amazonpay/config.js
@@ -6,7 +6,7 @@ export const amazonPayConfig = (props) => {
         showPayButton: true,
         productType: 'PayAndShip',
         checkoutMode: 'ProcessOrder',
-        returnUrl: `${window.location.href}/redirect`,
+        returnUrl: props.returnUrl || `${window.location.href}/redirect`,
         onClick: async (resolve, reject) => {
             try {
                 const [onBillingSubmit] = props.beforeSubmit
