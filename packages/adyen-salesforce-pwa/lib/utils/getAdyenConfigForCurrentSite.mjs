cc40ac2af82a5e7ac25a80dafdb9b4a67b92d122
Shani <shani.singh@adyen.com>
2024-01-29 14:03:20 +0100

    fix: read adyen config from env variables

--- packages/adyen-salesforce-pwa/lib/utils/getAdyenConfigForCurrentSite.mjs
+++ packages/adyen-salesforce-pwa/lib/utils/getAdyenConfigForCurrentSite.mjs
@@ -1,7 +1,37 @@
-import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
+import Logger from "../api/controllers/logger.js";
 
-export const getAdyenConfigForCurrentSite = () => {
-  const {app: appConfig} = getConfig()
-  const currentSiteId = appConfig.commerceAPI.parameters.siteId
-  return appConfig.sites.find((site) => site.id === currentSiteId)?.adyen
+export const getAdyenConfigForCurrentSite = (currentSiteId) => {
+    return {
+        apiKey: setProperty(currentSiteId, ADYEN_ENV.ADYEN_API_KEY, true),
+        clientKey: setProperty(currentSiteId, ADYEN_ENV.ADYEN_CLIENT_KEY, true),
+        environment: setProperty(currentSiteId, ADYEN_ENV.ADYEN_ENVIRONMENT, true),
+        merchantAccount: setProperty(currentSiteId, ADYEN_ENV.ADYEN_MERCHANT_ACCOUNT, true),
+        systemIntegratorName: setProperty(currentSiteId, ADYEN_ENV.SYSTEM_INTEGRATOR_NAME, false),
+        webhookUser: setProperty(currentSiteId, ADYEN_ENV.ADYEN_WEBHOOK_USER, false),
+        webhookPassword: setProperty(currentSiteId, ADYEN_ENV.ADYEN_WEBHOOK_PASSWORD, Object.hasOwn(process.env, `${currentSiteId}_${ADYEN_ENV.ADYEN_WEBHOOK_USER}`)),
+        webhookHmacKey: setProperty(currentSiteId, ADYEN_ENV.ADYEN_HMAC_KEY, false)
+    }
+}
+
+const setProperty = (currentSiteId, property, isRequired) => {
+    const siteEnv = `${currentSiteId}_${property}`
+    if (Object.hasOwn(process.env, siteEnv)) {
+        return process.env[siteEnv]
+    } else if (isRequired) {
+        const errorMessage = `${currentSiteId}_${property} is not defined in environment variables`
+        Logger.error('SET ADYEN CONFIG FOR SITE', errorMessage)
+        throw new Error(errorMessage)
+    }
+    return ''
+}
+
+const ADYEN_ENV = {
+    ADYEN_API_KEY: 'ADYEN_API_KEY',
+    ADYEN_CLIENT_KEY: 'ADYEN_CLIENT_KEY',
+    ADYEN_ENVIRONMENT: 'ADYEN_ENVIRONMENT',
+    ADYEN_MERCHANT_ACCOUNT: 'ADYEN_MERCHANT_ACCOUNT',
+    SYSTEM_INTEGRATOR_NAME: 'SYSTEM_INTEGRATOR_NAME',
+    ADYEN_WEBHOOK_USER: 'ADYEN_WEBHOOK_USER',
+    ADYEN_WEBHOOK_PASSWORD: 'ADYEN_WEBHOOK_PASSWORD',
+    ADYEN_HMAC_KEY: 'ADYEN_HMAC_KEY'
 }
