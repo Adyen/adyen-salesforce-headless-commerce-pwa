46c319f8e4a35f2d6a9ba8d2fc93ab4ee7700866
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2024-03-08 08:46:50 +0100

    Feature/sfi 628 demo site improvements (#123)
    * feat: remove all sites except US
    
    * feat: customize affirm and ach
    
    * feat: enable cashapp

--- packages/adyen-retail-react-app/overrides/app/routes.jsx
+++ packages/adyen-retail-react-app/overrides/app/routes.jsx
@@ -34,35 +34,15 @@ const fallback = <Skeleton height="75vh" width="100%" />
  * - Translations
  * - Payment Methods
  * - Execute Callbacks
- *
- * const checkoutCustomizations = {
- *     beforeSubmit: [
- *         function logAfterSubmit() {
- *             console.log('before submit')
- *             return true
- *         }
- *     ],
- *     afterSubmit: [
- *         function logAfterSubmit() {
- *             console.log('after submit')
- *             return true
- *         }
- *     ],
- *     translations: {
- *         'en-US': {
- *             payButton: ''
- *         }
- *     },
- *     paymentMethodsConfiguration: {
- *         paypal: {
- *             style: {
- *                 layout: 'vertical',
- *                 color: 'blue'
- *             }
- *         }
- *     }
- * }
  */
+
+const checkoutCustomizations = {
+    paymentMethodsConfiguration: {
+        klarna_account: {
+            useKlarnaWidget: false
+        }
+    }
+}
 
 // Create your pages here and add them to the routes array
 // Use loadable to split code into smaller js chunks
@@ -77,7 +57,7 @@ const Checkout = loadable(() => import('@adyen/adyen-salesforce-pwa'), {
                 useCustomerType={useCustomerType}
                 useShopperBasketsMutation={useShopperBasketsMutation}
                 useMultiSite={useMultiSite}
-                // adyenConfig={checkoutCustomizations}
+                adyenConfig={checkoutCustomizations}
             />
         )
     }
