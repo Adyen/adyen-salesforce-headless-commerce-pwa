4e9b11234a0d7329d1b129dc646df51aee965af9
shani <shani.singh@adyen.com>
2025-11-03 08:43:42 +0100

    test(SFI-1375): e2e test for gift cards

--- packages/e2e-tests/tests/giftCard.spec.js
+++ packages/e2e-tests/tests/giftCard.spec.js
@@ -5,17 +5,23 @@ import {PaymentHelper} from '../helpers/PaymentHelper.js'
 import {CardData} from '../data/cardData.js'
 
 const user_US = new ShopperData().US
+const giftCard = new CardData().giftCard
 const threeDs2 = new CardData().threeDs2
-const storedCard = new CardData().storedCard
+
 
 test.describe('Payments through PWA UI', () => {
-    test('CC 3Ds2 payments should succeed', async ({page}) => {
+    test('gift card payments should succeed', async ({page}) => {
         const scenarios = new ScenarioHelper(page)
         await scenarios.visitStore()
         await scenarios.setupCart()
 
         await scenarios.arrangeShippingAndProceedToPayment(user_US)
         const paymentPage = new PaymentHelper(page)
+        await paymentPage.selectPaymentType(giftCard.brand)
+        await paymentPage.fillGiftCardInfo(giftCard.cardNumber, giftCard.pin)
+        await paymentPage.clickPay()
+        await paymentPage.addedGiftCardIsDisplayed()
+
         await paymentPage.selectPaymentType('Cards')
         await paymentPage.fillCreditCardInfo(
             user_US.shopperName,
@@ -25,23 +31,6 @@ test.describe('Payments through PWA UI', () => {
         )
         await paymentPage.clickPay()
         await paymentPage.validate3DS2('password')
-        await scenarios.verifySuccessfulOrder()
-    })
-
-    test('Tokenized payment should succeed', async ({page}) => {
-        const scenarios = new ScenarioHelper(page)
-        await scenarios.login(user_US)
-        await scenarios.visitStore()
-        await page.reload();
-
-        await scenarios.setupCart()
-        await scenarios.arrangeShippingAndProceedToPaymentForLoggedInUser(user_US)
-
-        const paymentPage = new PaymentHelper(page)
-        await paymentPage.fillCVCInfo(
-            storedCard.cvc
-        )
-        await paymentPage.clickPay()
         await scenarios.verifySuccessfulOrder()
     })
 })
