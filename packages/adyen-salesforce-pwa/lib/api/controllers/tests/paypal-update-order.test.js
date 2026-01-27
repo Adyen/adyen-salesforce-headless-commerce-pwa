b200594ab092907607a346910c47b245ce567fb7
aleksandarm <aleksandar.mihajlovski@adyen.com>
2026-01-27 11:45:36 +0100

    fix: unit tests

--- packages/adyen-salesforce-pwa/lib/api/controllers/tests/paypal-update-order.test.js
+++ packages/adyen-salesforce-pwa/lib/api/controllers/tests/paypal-update-order.test.js
@@ -157,7 +157,8 @@ describe('paypalUpdateOrder controller', () => {
                     }
                 },
                 {
-                    idempotencyKey: 'test-uuid-1234'
+                    idempotencyKey:
+                        '50a1a1fe65e0dea7238d52998dd77b7f903597f1587845ac02d782f9bf2aeaea'
                 }
             )
 
