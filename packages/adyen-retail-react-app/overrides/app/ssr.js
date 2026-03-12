e1459cb5cbd83074ffe12f13efb03f309d07f2b8
Shani <shani.singh@adyen.com>
2026-03-12 16:04:21 +0100

    fix: add Apple CDN to CSP for Apple Pay support (#265)
    * fix: add Apple CDN to CSP for Apple Pay support

--- packages/adyen-retail-react-app/overrides/app/ssr.js
+++ packages/adyen-retail-react-app/overrides/app/ssr.js
@@ -51,7 +51,8 @@ const {handler} = runtime.createHandler(options, (app) => {
                         '*.payments-amazon.com',
                         'https://www.paypalobjects.com/js-sdk-logos/2.2.7/paypal-blue.svg',
                         'https://*.cash.app',
-                        'https://*.gstatic.com'
+                        'https://*.gstatic.com',
+                        '*.cdn-apple.com'
                     ],
                     'script-src': [
                         "'self'",
@@ -78,7 +79,8 @@ const {handler} = runtime.createHandler(options, (app) => {
                         'https://www.sandbox.paypal.com/xoplatform/logger/api/logger?disableSetCookie=true',
                         '*.google.com',
                         '*.mastercard.com',
-                        '*.visa.com'
+                        '*.visa.com',
+                        '*.cdn-apple.com'
                     ],
                     'frame-src': [
                         "'self'",
@@ -87,7 +89,8 @@ const {handler} = runtime.createHandler(options, (app) => {
                         '*.google.com',
                         'https://sandbox.kit.cash.app',
                         '*.mastercard.com',
-                        '*.visa.com'
+                        '*.visa.com',
+                        '*.cdn-apple.com'
                     ],
                     /* -----------------Adyen End ------------------------ */
                     // Do not upgrade insecure requests for local development
