8cca7404d1fdb434c469920fc956b6e334448eb5
aleksandarm <aleksandar.mihajlovski@adyen.com>
2026-03-18 14:38:26 +0100

    fix: prevent errors if no payment instrument

--- packages/int_adyen_PWA/cartridge/rest-apis/adyen-order/updateOrderPaymentInstrument.js
+++ packages/int_adyen_PWA/cartridge/rest-apis/adyen-order/updateOrderPaymentInstrument.js
@@ -13,7 +13,7 @@ exports.updateOrderPaymentInstrument = function () {
     const orderNo = match ? match[1] : null;
     const requestBody = request.httpParameterMap.requestBodyAsString;
     const allowedCustomProperties = ['donationToken'];
-    const {pspReference, customProperties} = JSON.parse(requestBody);
+    const {pspReference, customProperties = {}} = JSON.parse(requestBody);
     if (!orderNo) {
       RESTResponseMgr.createError(400, 'bad_request', 'Missing orderNo parameter').render();
       return;
@@ -22,10 +22,14 @@ exports.updateOrderPaymentInstrument = function () {
     const order = OrderMgr.getOrder(orderNo);
     if (order) {
       const paymentInstrument = order.paymentInstruments.toArray().find((pi) => pi.custom.pspReference === pspReference);
+      if (!paymentInstrument) {
+        RESTResponseMgr.createError(404, 'not_found', 'Payment instrument not found').render();
+        return;
+      }
       allowedCustomProperties.forEach((prop) => {
         paymentInstrument.custom[prop] = customProperties[prop];
-      })
-      RESTResponseMgr.createSuccess(response, 200).render();
+      });
+      RESTResponseMgr.createSuccess({}, 200).render();
     } else {
       RESTResponseMgr.createError(404, 'not_found', 'Order not found').render();
     }
