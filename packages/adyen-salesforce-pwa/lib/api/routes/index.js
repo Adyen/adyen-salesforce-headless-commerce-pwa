ff1e9b3a46ac035c19346acf5f50e7de6d055ec4
aleksandarm <aleksandar.mihajlovski@adyen.com>
2026-07-03 11:50:33 +0200

    chore: temp change to apple pay

--- packages/adyen-salesforce-pwa/lib/api/routes/index.js
+++ packages/adyen-salesforce-pwa/lib/api/routes/index.js
@@ -55,7 +55,6 @@ function registerAdyenEndpoints(app, runtime, overrides, options = {}) {
     const paymentsDetailsContext = createPaymentsDetailsContext(options)
 
     const appleDomainAssociationHandler = overrides?.appleDomainAssociation || [
-        minimalRequestContext,
         appleDomainAssociation,
         ErrorHandler
     ]
