664fa47ec4f40ddae1a7948d139895b10c42a2ec
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2026-07-14 11:40:10 +0200

    fix: lint

--- packages/adyen-salesforce-pwa/lib/components/tests/adyenCheckout.test.js
+++ packages/adyen-salesforce-pwa/lib/components/tests/adyenCheckout.test.js
@@ -686,6 +686,7 @@ describe('AdyenCheckoutComponent', () => {
                 '',
                 '/checkout/redirect?redirectResult=xyz&adyenExpressBasketId=express-basket-1'
             )
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const {basket: _unusedBasket, ...propsWithoutBasket} = defaultProps
 
             render(<AdyenCheckoutComponent {...propsWithoutBasket} page="redirect" />)
@@ -701,6 +702,7 @@ describe('AdyenCheckoutComponent', () => {
 
         it('falls back to the current basket when there is no express basket id in the URL', async () => {
             window.history.pushState({}, '', '/checkout/redirect?redirectResult=xyz')
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const {basket: _unusedBasket, ...propsWithoutBasket} = defaultProps
 
             render(<AdyenCheckoutComponent {...propsWithoutBasket} page="redirect" />)
