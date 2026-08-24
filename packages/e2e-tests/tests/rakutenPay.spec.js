045d78f065dd90dafedd0e9d6e9746c62fc2a08e
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2026-08-24 09:50:29 +0200

    test: add Rakuten Pay e2e rendering test for Japanese locale

---
+++ packages/e2e-tests/tests/rakutenPay.spec.js
@@ -0,0 +1,23 @@
+import {test} from '@playwright/test'
+import {ScenarioHelper} from '../helpers/ScenarioHelper.js'
+import {ShopperData} from '../data/shopperData.js'
+import {PaymentHelper} from '../helpers/PaymentHelper.js'
+import {LocaleData} from '../data/localeData'
+
+const user_JP = new ShopperData().JP
+
+test.describe('Payments through PWA UI', () => {
+    test.beforeEach(async ({page}) => {
+        const scenarios = new ScenarioHelper(page, new LocaleData().JP)
+        await scenarios.visitStore()
+        await scenarios.setupCart()
+    })
+
+    test('Rakuten Pay should render on checkout page for Japanese locale', async ({page}) => {
+        const scenarios = new ScenarioHelper(page, new LocaleData().JP)
+        await scenarios.arrangeShippingAndProceedToPayment(user_JP)
+        const paymentPage = new PaymentHelper(page)
+        await paymentPage.selectPaymentType('Rakuten Pay')
+        await paymentPage.rakutenPayIsRendered()
+    })
+})
